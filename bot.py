adf6742497d7b01e2ec444d651db8544aceb52d8
dOtOb9 <dotob9@gmail.com>
2024-06-18 22:07:53 +0900

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,44 @@
+import discord
+from os import getenv
+
+from main import send_image_to_ai
+
+intents = discord.Intents.default()
+intents.members = True
+
+bot = discord.Bot(intents=intents)
+
+
+@bot.event
+@bot.event
+async def on_message(message: discord.Message):
+    if message.author.bot: return
+    
+    if message.channel.type == 'private': return  # DM以外のチャンネルでのメッセージは無視
+
+
+    if message.attachments != []:
+        for attachment in message.attachments:
+            virtual_water_text, items_text = send_image_to_ai(attachment.url)
+
+            embed = discord.Embed(
+                title='仮想水算出結果', 
+                colour=discord.Colour.green(),
+                fields=[
+                    discord.EmbedField(
+                        name='品名', 
+                        value=items_text
+                    ),
+                    discord.EmbedField(
+                        name='仮想水量', 
+                        value=virtual_water_text
+                    )
+                ]
+            )
+
+            embed.set_footer(text='gpt-4o-2024-05-13', icon_url='chatgpt-logo.png')
+
+    await message.channel.send(embed=embed)
+
+
+bot.run(getenv('DISCORD_BOT_TOKEN'))
